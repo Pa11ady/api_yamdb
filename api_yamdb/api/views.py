2945e8c37b55722bd06f5b6cebe39f932acf881a
Pavel <pa11ady@yandex.ru>
2024-06-20 17:11:52 +0300

    views (review, comment)

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+
+from reviews.models import Review, Title
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewViewSet(ModelViewSet):
+    http_method_names = ('get', 'patch', 'post', 'delete')
+    serializer_class = ReviewSerializer
+
+    def get_title(self):
+        return get_object_or_404(Title, pk=self.kwargs['title_id'])
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user,
+                               title=self.get_title())
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+
+class CommentViewSet(ModelViewSet):
+    http_method_names = ('get', 'patch', 'post', 'delete')
+    serializer_class = CommentSerializer
+
+    def get_review(self):
+        return get_object_or_404(Review, pk=self.kwargs['review_id'],
+                                 title__id=self.kwargs['title_id'])
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user,
+                               review=self.get_review())
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
