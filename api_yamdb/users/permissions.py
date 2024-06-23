efd2aeca4b650524e0dd084447340026e0d9890e
Асылгареев Вадим <710271119@mail.ru>
2024-06-24 00:15:29 +0500

    tests fix

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -10,19 +10,21 @@ class AnonReadOnly(permissions.BasePermission):
 class IsSuperUserOrAdminAndIsAuth(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (request.user.is_superuser
-                or request.user.is_staff
-                or request.user.is_admin)
+        if request.user.is_authenticated:
+            return (request.user.is_superuser
+                    or request.user.is_staff
+                    or request.user.is_admin)
 
 
 class IsSuperUserOrAdminOrModerOrAuthorAndIsAuth(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or (request.user.is_superuser
-                or request.user.is_staff
-                or request.user.is_admin
-                or request.user.is_moderator
-                or request.user == obj.author)
-        )
+        if request.user.is_authenticated:
+            return (
+                request.method in permissions.SAFE_METHODS
+                or (request.user.is_superuser
+                    or request.user.is_staff
+                    or request.user.is_admin
+                    or request.user.is_moderator
+                    or request.user == obj.author)
+                    )
