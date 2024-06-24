da9b75cd0c7597554fa81d0c7e64b803a6e7897c
rawmeraw <rawmeraw@yandex.ru>
2024-06-24 17:26:39 +0500

    Removed the staff role

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -11,20 +11,13 @@ class IsSuperUserOrAdminAndIsAuth(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.user.is_authenticated:
-            return (request.user.is_superuser
-                    or request.user.is_staff
-                    or request.user.is_admin)
+            return (request.user.is_superuser or request.user.is_admin)
 
 
 class IsSuperUserOrAdminOrModerOrAuthorAndIsAuth(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated:
-            return (
-                request.method in permissions.SAFE_METHODS
-                or (request.user.is_superuser
-                    or request.user.is_staff
-                    or request.user.is_admin
+        return (request.method in permissions.SAFE_METHODS
+                or (request.user.is_superuser or request.user.is_admin
                     or request.user.is_moderator
-                    or request.user == obj.author)
-                    )
+                    or request.user == obj.author))
