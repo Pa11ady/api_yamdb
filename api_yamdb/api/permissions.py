a6fe4d5583b572076bd7cf16a1101068d341166c
rawmeraw <rawmeraw@yandex.ru>
2024-06-27 11:57:01 +0500

    Change many or for any

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,22 +3,29 @@ from rest_framework import permissions
 
 class AdminPermission(permissions.BasePermission):
     """Пермишн для доступа только админу или суперюзеру."""
+
     def has_permission(self, request, view):
         user = request.user
         if request.method in permissions.SAFE_METHODS:
             return True
         if user.is_authenticated:
-            return (user.is_admin or user.is_superuser)
+            return user.is_admin or user.is_superuser
 
 
 class AdminOrAuthorOrReadOnly(permissions.BasePermission):
     """Пермишн для доступа модератору, админу или автору."""
+
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.method in permissions.SAFE_METHODS or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_admin or request.user.is_moderator
-                or request.user.is_superuser
-                or request.user == obj.author)
+        return request.method in permissions.SAFE_METHODS or any(
+            (
+                request.user.is_admin,
+                request.user.is_moderator,
+                request.user.is_superuser,
+                request.user == obj.author,
+            )
+        )
